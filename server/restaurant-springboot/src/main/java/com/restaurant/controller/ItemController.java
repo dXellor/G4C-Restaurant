9b9801f89fa6985a12ba1e6f74893d9272742942
Nikola Simic <nikola7simic@gmail.com>
2022-09-23 11:47:38 +0200

    added CrossOrigin to controllers

--- server/restaurant-springboot/src/main/java/com/restaurant/controller/ItemController.java
+++ server/restaurant-springboot/src/main/java/com/restaurant/controller/ItemController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/restaurant/items")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ItemController {
 
     @Autowired
